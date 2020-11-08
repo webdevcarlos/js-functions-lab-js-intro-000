bb4cd04d1cff70d3926e89cba6d8e34c73c83def
inherited-checkpoint-3530 <webdevcarlosferreira@gmail.com>
2020-11-08 23:40:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // write your code below!
 function happyHolidays(){
   console.log('Happy holidays!');
-};
+}
 happyHolidays();
 function happyHolidays(name){
   console.log(`Happy holidays, ${name}!`);
-};
+}
 happyHolidaysTo ("Carlos");
 function happyCustomHolidayTo(holiday,name){
   console.log(`Happy ${holiday}, ${name}!`);
-};
+}
 happyCustomHolidayTo ("Halloween","Carlos");
 function holidayCountdown(holiday,days){
   console.log (`It's ${days} days until ${holiday}!`);
-};
+}
 holidayCountdown ("Halloween",2);
